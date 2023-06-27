8dc5c5d2608940b838804ef8979287ac4fc60e46
Aryan20apr <aryansingh20nov@gmail.com>
2023-06-28 01:12:54 +0530

    AuthenticationManager Bean

--- src/main/java/com/aryan/examportal_backend/config/SecurityConfig.java
+++ src/main/java/com/aryan/examportal_backend/config/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,7 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.DefaultSecurityFilterChain;
+
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -85,11 +85,11 @@ public class SecurityConfig {
 
 	    }
 
-	    // @Bean
-	    // public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception {
-	    //     // Required to authenticate the password
-	    //     return configuration.getAuthenticationManager();
-	    // }
+	    @Bean
+	    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception {
+	        // Required to authenticate the password
+	        return configuration.getAuthenticationManager();
+	    }
 
 		
 		  @Bean public PasswordEncoder passwordEncoder() { return new
